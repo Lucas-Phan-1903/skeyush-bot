05e796a739be9929c9e9ea0c5db2c6623c924729
PreranaCodes <inspiresprerana@gmail.com>
2022-06-11 13:04:15 +0530

    Fixed few error still left

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-// Require the necessary discord.js classes
-const { token } = require('./config.json');
+//Require the necessary discord.js classes
+require {token} from './config.json'
 
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+    partials: ["MESSAGE", "CHANNEL", "REACTION"],
+    intents: 32767,
+});
+const {Client} = require ('discord.js');
 
 
 
-// When the client is ready, run this code (only once)
+
+//WHEN the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
 });
@@ -20,7 +25,7 @@ client.on('interactionCreate', async interaction => {
 	if (commandName === 'ping') {
 		await interaction.reply('Pong!');
 	} else if (commandName === 'server') {
-        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
 	} else if (commandName === 'user') {
 		await interaction.reply('User info.');
 	}
@@ -29,5 +34,5 @@ client.on('interactionCreate', async interaction => {
 
 
 
-// Login to Discord with your client's token
+//Login to Discord with your client's token
 client.login(token);
